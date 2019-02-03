f8635502378a53636a1c4d118e6572ba5d9b9985
Joaquin Culasso <joaquin.culasso@gmail.com>
2019-02-03 18:51:15 -0300

    versionInicial

---
+++ Campeonato/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace Campeonato
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClean = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.operacionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reporteEquipoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.acercaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblBirthDate = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.lblTeam = new System.Windows.Forms.Label();
+            this.lblTeamList = new System.Windows.Forms.ComboBox();
+            this.lblDni = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.mtxtAge = new System.Windows.Forms.MaskedTextBox();
+            this.mtxtDni = new System.Windows.Forms.MaskedTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.prgbQuality = new System.Windows.Forms.ProgressBar();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(111, 35);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(200, 20);
+            this.txtFirstName.TabIndex = 0;
+            this.txtFirstName.TextChanged += new System.EventHandler(this.txtFirstName_TextChanged);
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(12, 42);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(44, 13);
+            this.lblFirstName.TabIndex = 2;
+            this.lblFirstName.Text = "Nombre";
+            this.lblFirstName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(111, 71);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(200, 20);
+            this.txtLastName.TabIndex = 1;
+            this.txtLastName.TextChanged += new System.EventHandler(this.txtLastName_TextChanged);
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(12, 78);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(44, 13);
+            this.lblLastName.TabIndex = 4;
+            this.lblLastName.Text = "Apellido";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(374, 268);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Guardar";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClean
+            // 
+            this.btnClean.Location = new System.Drawing.Point(456, 268);
+            this.btnClean.Name = "btnClean";
+            this.btnClean.Size = new System.Drawing.Size(75, 23);
+            this.btnClean.TabIndex = 7;
+            this.btnClean.Text = "Limpiar";
+            this.btnClean.UseVisualStyleBackColor = true;
+            this.btnClean.Click += new System.EventHandler(this.btnClean_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.operacionesToolStripMenuItem,
+            this.ayudaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(564, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(96, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // operacionesToolStripMenuItem
+            // 
+            this.operacionesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.reporteEquipoToolStripMenuItem});
+            this.operacionesToolStripMenuItem.Name = "operacionesToolStripMenuItem";
+            this.operacionesToolStripMenuItem.Size = new System.Drawing.Size(85, 20);
+            this.operacionesToolStripMenuItem.Text = "Operaciones";
+            // 
+            // reporteEquipoToolStripMenuItem
+            // 
+            this.reporteEquipoToolStripMenuItem.Name = "reporteEquipoToolStripMenuItem";
+            this.reporteEquipoToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
+            this.reporteEquipoToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
+            this.reporteEquipoToolStripMenuItem.Text = "Reporte Equipo";
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.acercaDeToolStripMenuItem});
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            this.acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.acercaDeToolStripMenuItem.Text = "Acerca de...";
+            this.acercaDeToolStripMenuItem.Click += new System.EventHandler(this.acercaDeToolStripMenuItem_Click);
+            // 
+            // lblBirthDate
+            // 
+            this.lblBirthDate.AutoSize = true;
+            this.lblBirthDate.Location = new System.Drawing.Point(12, 111);
+            this.lblBirthDate.Name = "lblBirthDate";
+            this.lblBirthDate.Size = new System.Drawing.Size(93, 13);
+            this.lblBirthDate.TabIndex = 8;
+            this.lblBirthDate.Text = "Fecha Nacimiento";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(12, 147);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(32, 13);
+            this.lblAge.TabIndex = 9;
+            this.lblAge.Text = "Edad";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "dd - MM - yyyy";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(111, 105);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(104, 20);
+            this.dateTimePicker1.TabIndex = 2;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // lblTeam
+            // 
+            this.lblTeam.AutoSize = true;
+            this.lblTeam.Location = new System.Drawing.Point(12, 225);
+            this.lblTeam.Name = "lblTeam";
+            this.lblTeam.Size = new System.Drawing.Size(40, 13);
+            this.lblTeam.TabIndex = 12;
+            this.lblTeam.Text = "Equipo";
+            // 
+            // lblTeamList
+            // 
+            this.lblTeamList.FormattingEnabled = true;
+            this.lblTeamList.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3"});
+            this.lblTeamList.Location = new System.Drawing.Point(111, 217);
+            this.lblTeamList.Name = "lblTeamList";
+            this.lblTeamList.Size = new System.Drawing.Size(121, 21);
+            this.lblTeamList.TabIndex = 5;
+            this.lblTeamList.Text = "Seleccione equipo";
+            this.lblTeamList.SelectedIndexChanged += new System.EventHandler(this.lblTeamList_SelectedIndexChanged);
+            // 
+            // lblDni
+            // 
+            this.lblDni.AutoSize = true;
+            this.lblDni.Location = new System.Drawing.Point(12, 185);
+            this.lblDni.Name = "lblDni";
+            this.lblDni.Size = new System.Drawing.Size(26, 13);
+            this.lblDni.TabIndex = 14;
+            this.lblDni.Text = "DNI";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // mtxtAge
+            // 
+            this.mtxtAge.Location = new System.Drawing.Point(111, 140);
+            this.mtxtAge.Mask = "99";
+            this.mtxtAge.Name = "mtxtAge";
+            this.mtxtAge.Size = new System.Drawing.Size(24, 20);
+            this.mtxtAge.TabIndex = 3;
+            this.mtxtAge.ValidatingType = typeof(int);
+            this.mtxtAge.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.mtxtAge_MaskInputRejected);
+            // 
+            // mtxtDni
+            // 
+            this.mtxtDni.Location = new System.Drawing.Point(111, 178);
+            this.mtxtDni.Mask = "99999999";
+            this.mtxtDni.Name = "mtxtDni";
+            this.mtxtDni.Size = new System.Drawing.Size(60, 20);
+            this.mtxtDni.TabIndex = 4;
+            this.mtxtDni.ValidatingType = typeof(int);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(374, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(157, 163);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // prgbQuality
+            // 
+            this.prgbQuality.Location = new System.Drawing.Point(374, 204);
+            this.prgbQuality.Name = "prgbQuality";
+            this.prgbQuality.Size = new System.Drawing.Size(157, 23);
+            this.prgbQuality.TabIndex = 16;
+            this.prgbQuality.Click += new System.EventHandler(this.prgbQuality_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 303);
+            this.Controls.Add(this.prgbQuality);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.mtxtDni);
+            this.Controls.Add(this.mtxtAge);
+            this.Controls.Add(this.lblDni);
+            this.Controls.Add(this.lblTeam);
+            this.Controls.Add(this.lblTeamList);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblBirthDate);
+            this.Controls.Add(this.btnClean);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClean;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem operacionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reporteEquipoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem acercaDeToolStripMenuItem;
+        private System.Windows.Forms.Label lblBirthDate;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label lblTeam;
+        private System.Windows.Forms.ComboBox lblTeamList;
+        private System.Windows.Forms.Label lblDni;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.MaskedTextBox mtxtDni;
+        private System.Windows.Forms.MaskedTextBox mtxtAge;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar prgbQuality;
+    }
+}
+
---
+++ Campeonato/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GriauleFingerprintLibrary;
+using GriauleFingerprintLibrary.Exceptions;
+
+namespace Campeonato
+{
+    public partial class Form1 : Form
+    {
+
+        private FingerprintCore fingerPrint;
+        private GriauleFingerprintLibrary.DataTypes.FingerprintRawImage rawImage;
+        GriauleFingerprintLibrary.DataTypes.FingerprintTemplate _template;
+
+        public Form1()
+        {
+            InitializeComponent();
+            fingerPrint = new FingerprintCore();
+            fingerPrint.onStatus += new StatusEventHandler(fingerPrint_onStatus);
+            //fingerPrint.onFinger += new FingerEventHandler(fingerPrint_onFinger);
+            fingerPrint.onImage += new ImageEventHandler(fingerPrint_onImage);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fingerPrint.Initialize();
+            fingerPrint.CaptureInitialize();
+        }
+
+        private void fontDialog1_Apply(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtFirstName_TextChanged(object sender, EventArgs e)
+        {
+
+            //recorremos el campo y verificamos quelos caracteres escritos sean correctos
+            bool error = false;
+
+            foreach (char caracter in txtFirstName.Text)
+            {
+                if (!char.IsLetter(caracter))
+                {
+                    error = true;
+                    break;
+
+                }
+            }
+
+            //Verificamos por la condicion de error
+            if (error)
+            {
+                errorProvider1.SetError(txtFirstName, "Solamente letras en el nombre");
+            }
+            else
+            {
+                errorProvider1.Clear();
+            }
+        }
+
+        private void txtLastName_TextChanged(object sender, EventArgs e)
+        {
+
+            //recorremos el campo y verificamos quelos caracteres escritos sean correctos
+            bool error = false;
+
+            foreach (char caracter in txtLastName.Text)
+            {
+                if (!char.IsLetter(caracter))
+                {
+                    error = true;
+                    break;
+                }
+            }
+
+
+            //Verificamos por la condicion de error
+            if (error)
+            {
+                errorProvider1.SetError(txtLastName, "Solamente letras en el apellido");
+            }
+            else
+            {
+                errorProvider1.Clear();
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+            using (var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["conexion"].ToString()))
+            {
+
+                var query = @"INSERT INTO persona (First_Name, Last_Name, Birth_Date, Age, Dni, Id_Team) VALUES (@First_Name, @Last_Name, @Birth_Date, @Age, @Dni, @Id_Team)";
+                SqlCommand command = new SqlCommand(query, conn);
+                command.Parameters.Add(new SqlParameter("@First_Name", txtFirstName.Text));
+                command.Parameters.Add(new SqlParameter("@Last_Name", txtLastName.Text));
+                command.Parameters.Add(new SqlParameter("@Birth_Date", dateTimePicker1.Value.ToString("yyyy/MM/dd")));
+                command.Parameters.Add(new SqlParameter("@Age", mtxtAge.Text));
+                command.Parameters.Add(new SqlParameter("@Dni", mtxtDni.Text));
+                command.Parameters.Add(new SqlParameter("@Id_Team", lblTeamList.Text));
+                conn.Open();
+                command.ExecuteNonQuery();
+                conn.Close();
+            }
+            MessageBox.Show("Se registró con éxito!!", "Programa con Joako", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void acercaDeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("App Campeonato v1.0", "Programa Joako", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtAge_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mtxtAge_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void btnClean_Click(object sender, EventArgs e)
+        {
+            txtFirstName.Clear();
+            txtLastName.Clear();
+            dateTimePicker1.ResetText();
+            mtxtAge.Clear();
+            mtxtDni.Clear();
+            lblTeamList.ResetText(); ;
+        }
+
+        private void lblTeamList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        void fingerPrint_onStatus(object source, GriauleFingerprintLibrary.Events.StatusEventArgs se)
+        {
+            if (se.StatusEventType == GriauleFingerprintLibrary.Events.StatusEventType.SENSOR_PLUG)
+            {
+                fingerPrint.StartCapture(source.ToString());
+            }
+            else
+            {
+                fingerPrint.StartCapture(source);
+            }
+        }
+
+        void fingerPrint_onImage(object source, GriauleFingerprintLibrary.Events.ImageEventArgs ie)
+        {
+            rawImage = ie.RawImage;
+            SetImage(ie.RawImage.Image);
+
+            ExtractTemplate();
+        }
+
+        private delegate void delSetImage(Image img);
+        void SetImage(Image img)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new delSetImage(SetImage), new object[] { img });
+            }
+            else
+            {
+                Bitmap bmp = new Bitmap(img, pictureBox1.Width, pictureBox1.Height);
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private void ExtractTemplate()
+        {
+            if (rawImage != null)
+            {
+                try
+                {
+                    _template = null;
+                    fingerPrint.Extract(rawImage, ref _template);
+
+                    SetQualityBar(_template.Quality);
+
+                    DisplayImage(_template, false);
+                }
+                catch
+                {
+                    SetQualityBar(-1);
+                }
+            }
+        }
+
+        delegate void delsetQuality(int quality);
+        private void SetQualityBar(int quality)
+        {
+            if (prgbQuality.InvokeRequired == true)
+            {
+                this.Invoke(new delsetQuality(SetQualityBar), new object[] { quality });
+            }
+            else
+            {
+                switch (quality)
+                {
+                    case 0:
+                        {
+                            //prgbQuality.ProgressBarColor = System.Drawing.Color.LightCoral;
+                            prgbQuality.Value = prgbQuality.Maximum / 3;
+                        }
+                        break;
+                    case 1:
+                        {
+                            //prgbQuality.ProgressBarColor = System.Drawing.Color.YellowGreen;
+                            prgbQuality.Value = (prgbQuality.Maximum / 3) * 2;
+                        }
+                        break;
+                    case 2:
+                        {
+                            //prgbQuality.ProgressBarColor = System.Drawing.Color.MediumSeaGreen;
+                            prgbQuality.Value = prgbQuality.Maximum;
+                        }
+                        break;
+                    default:
+                        {
+                            //prgbQuality.ProgressBarColor = System.Drawing.Color.Gray;
+                            prgbQuality.Value = 0;
+                        }
+                        break;
+                }
+            }
+        }
+
+        private void DisplayImage(GriauleFingerprintLibrary.DataTypes.FingerprintTemplate template, bool identify)
+        {
+            IntPtr hdc = FingerprintCore.GetDC();
+            IntPtr image = new IntPtr();
+
+            if (identify)
+            {
+                fingerPrint.GetBiometricDisplay(template, rawImage, hdc, ref image, FingerprintConstants.GR_DEFAULT_CONTEXT);
+                //button4.Enabled = true;
+            }
+            else
+            {
+                fingerPrint.GetBiometricDisplay(template, rawImage, hdc, ref image, FingerprintConstants.GR_NO_CONTEXT);
+                //button2.Enabled = true;
+            }
+
+            SetImage(Bitmap.FromHbitmap(image));
+            FingerprintCore.ReleaseDC(hdc);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void prgbQuality_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
